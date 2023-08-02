7d2b4dcf17b7c725d5422df8d8d5fd15482ac699
ngozionthewebs <231285@virtualwindow.co.za>
2023-08-02 14:14:22 +0200

    homework done

---
+++ js/index.js
@@ -0,0 +1,9 @@
+//When the document loads
+$(document).ready(function(){
+
+    console.log("Hello")
+
+    // When the Hero image loads animate it upwards
+
+    $(".hero-image").animate({top: '-=100'})
+})
